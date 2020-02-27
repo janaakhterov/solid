1a569f576f3e98fbc276634897ca26ef95c38eb2
Daniel Akhterov <akhterovd@gmail.com>
2020-02-26 23:30:34 -0800

    Add documentation to front of crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,61 @@
+//! ### Basic usage of the crate without the "serde" nor "dervie" features would be using `Builder` and `Selector`.
+//!
+//! ```rust
+//! let function = Builder::new()
+//!     .name("transfer")
+//!     .push("daniel")
+//!     .push(10u128)
+//!     .push(Bytes10([1u8; 10]))
+//!     .build();
+//! ```
+//!
+//! ### Usage with the "derive" feature would look like.
+//!
+//! Use `#[solidity(contstructor)]` if you're constructing a contract.
+//! Otherwise the name of the struct and the field types will be used
+//! to derive the function signature.
+//!
+//! ```rust
+//! #[derive(Encode)]
+//! #[solidity(rename = "random_function")]
+//! struct ContractCallComposite<'a> {
+//!     to: (&'a str, u128),
+//!     memos: &'a [&'a str],
+//!     matrix: &'a [&'a [&'a [u8]]],
+//! }
+//!
+//! #[derive(Decode)]
+//! struct ContractCallResponse<'a> {
+//!     int: Uint256,
+//!     bytes: Bytes<'a>,
+//!     memo: &'a str,
+//!     address: Address,
+//! }
+//! ```
+//!
+//! ### Usage with the "serde" feature would look like.
+//!
+//! ```rust
+//! #[derive(Serialize)]
+//! #[solidity(rename = "random_function")]
+//! struct ContractCallComposite<'a> {
+//!     to: (&'a str, u128),
+//!     memos: &'a [&'a str],
+//!     matrix: &'a [&'a [&'a [u8]]],
+//! }
+//!
+//! #[derive(Deserialize)]
+//! struct ContractCallResponse<'a> {
+//!     // Uint256 is not supported.
+//!     // int: Uint256,
+//!     int: u128,
+//!     bytes: Bytes<'a>,
+//!     memo: &'a str,
+//!     // Address is not supported.
+//!     // address: Address,
+//! }
+//! ```
+//!
 #[cfg(feature = "derive")]
 pub use solidity_derive as derive;
 
